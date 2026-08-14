4a6bd0f8b2d60aa6323ff54b1d3752975e1ec824
sivchhengkheang <kheangsivechheng@gmail.com>
2026-08-14 22:03:10 +0700

    config api

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "https://cms-rbac-server.onrender.com";
+
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
 
 const storageKey = "cms-rbac-auth";
 
